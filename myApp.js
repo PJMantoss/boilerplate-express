bea5d46727c7ff91e53b48d6ee3cb9937db0d71d
PJMantoss <joelptoss@gmail.com>
2021-06-16 15:43:50 +0100

    update myApp

--- myApp.js
+++ myApp.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
-let path = __dirname + /views/index.html;
+//let path = __dirname + '/views/index.html';
+let path = __dirname + '/public';
 
 app.get("/", (req, res) => {
     res.sendFile(path);
