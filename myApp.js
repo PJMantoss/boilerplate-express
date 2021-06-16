8bca7d399f815231e691c1a371f3e950124d8901
PJMantoss <joelptoss@gmail.com>
2021-06-16 15:06:06 +0100

    update myApp

--- myApp.js
+++ myApp.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get("/", (req, res) => {
-    res.end(console.log("Hello World"))
+    res.send("Hello World");
 })
 
  module.exports = app;
