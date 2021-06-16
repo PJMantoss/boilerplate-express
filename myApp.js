eba63476e2f70002e8f9614b17d7e0ae4a2dbbc9
PJMantoss <joelptoss@gmail.com>
2021-06-16 18:38:07 +0100

    update myApp

--- myApp.js
+++ myApp.js
@@ -3,19 +3,18 @@ var app = express();
 let path = __dirname + '/views/index.html';
 let publicPath = __dirname + '/public';
 
-let hiJSON = {"message": "Hello json"};
-let hiJSON2 = {"message": "HELLO JSON"};
-
 app.use(express.static(publicPath));
 
 // Assets at the /public route
 app.use("/public", express.static(publicPath));
 
 app.get("/json", (req, res) => {
-    if(process.env.MESSAGE_STYLE === uppercase){
-        res.json(hiJSON);
+  const mySecret = process.env['MESSAGE_STYLE']
+
+    if(mySecret === "uppercase"){
+        res.json({"message": "Hello json".toUpperCase()});
     }else{
-        res.json(hiJSON2);
+        res.json({"message": "Hello json"});
     }
 })
 
