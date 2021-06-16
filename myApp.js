feb15d96c1248af9ecedd574e48ebaccef1372d2
PJMantoss <joelptoss@gmail.com>
2021-06-16 17:40:06 +0100

    update myApp

--- myApp.js
+++ myApp.js
@@ -3,13 +3,15 @@ var app = express();
 let path = __dirname + '/views/index.html';
 let publicPath = __dirname + '/public';
 
+let hiJSON = {"message": "Hello json"};
+
 app.use(express.static(publicPath));
 
 // Assets at the /public route
 app.use("/public", express.static(publicPath));
 
-app.get("/", (req, res) => {
-    res.sendFile(path);
+app.get("/json", (req, res) => {
+    res.json(hiJSON);
 })
 
 module.exports = app;
