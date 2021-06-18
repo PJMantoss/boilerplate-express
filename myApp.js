bbcd5db540fd2b2c3636cfe5d0706aab0ffeefe7
PJMantoss <joelptoss@gmail.com>
2021-06-18 13:24:47 +0100

    update myApp

--- myApp.js
+++ myApp.js
@@ -4,7 +4,7 @@ let path = __dirname + '/views/index.html';
 let publicPath = __dirname + '/public';
 
 //Challenge 5 - root-level request logger middleware
-app.use(function(req, res, next){
+app.use((req, res, next) => {
     console.log(`${req.method} ${req.path} - ${req.ip} `);
     next();
   })
