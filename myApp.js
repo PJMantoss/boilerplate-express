d1b4a52df6cbcf5608b74c85a2373b1b9f94dd66
PJMantoss <joelptoss@gmail.com>
2021-06-19 11:45:09 +0100

    update comments in myApp

--- myApp.js
+++ myApp.js
@@ -5,10 +5,10 @@ let bodyParser = require('body-parser');
 let path = __dirname + '/views/index.html';
 let publicPath = __dirname + '/public';
 
-//Challenge 9 - Use body-parser to Parse POST Requests
+//Challenge - Use body-parser to Parse POST Requests
 app.use(bodyParser.urlencoded({ extended: false }));
 
-//Challenge 5 - root-level request logger middleware
+//Challenge - root-level request logger middleware
 app.use((req, res, next) => {
     console.log(`${req.method} ${req.path} - ${req.ip} `);
     next();
@@ -19,17 +19,17 @@ app.use(express.static(publicPath));
 // Assets at the /public route
 app.use("/public", express.static(publicPath));
 
-//Challenge 8 - Get Query Parameter Input from the Client
+//Challenge - Get Query Parameter Input from the Client
 app.get("/name", (req, res) => {
     res.send({name: req.query.first + " " + req.query.last});
 })
 
-//Challenge 7 - Get Route Parameter Input from the Client
+//Challenge - Get Route Parameter Input from the Client
 app.get("/:word/echo", (req, res) => {
     res.send({echo: req.params.word});
 })
 
-//Challenge 6 - Chain Middleware to Create a Time Server
+//Challenge - Chain Middleware to Create a Time Server
 app.get("/now", (req, res, next) => {
     req.time = new Date().toString();
     next();
